52255d302bafac8642d13fce6b7b0f6cd0482803
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2022-08-07 12:56:14 +0800

    init

---
+++ src/main/java/com/example/oracledbdemo/OracledbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oracledbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracledbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracledbDemoApplication.class, args);
+	}
+
+}
