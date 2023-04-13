0667195928101a09aa9944fc0a950daec8bf2872
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-04-13 14:21:34 +0800

    SpelTest

--- src/test/java/com/example/oracledbdemo/SpelTest.java
+++ src/test/java/com/example/oracledbdemo/SpelTest.java
@@ -3,6 +3,7 @@ package com.example.oracledbdemo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
@@ -17,56 +18,87 @@ import com.google.zxing.WriterException;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class SpelTest {
 
-        // https://www.baeldung.com/spring-expression-language
-        // https://stackoverflow.com/questions/67414561/spring-expression-language-referencing-variables
-        @Test
-        void test1() throws WriterException, IOException {
-                ExpressionParser expressionParser = new SpelExpressionParser();
-                Expression expression = expressionParser.parseExpression("'Any string'");
-                String result = (String) expression.getValue();
-                System.out.println(result);
+    // https://www.baeldung.com/spring-expression-language
+    // https://stackoverflow.com/questions/67414561/spring-expression-language-referencing-variables
+    // https://docs.spring.io/spring-framework/docs/3.2.x/spring-framework-reference/html/expressions.html
+    @Test
+    void test1() throws WriterException, IOException {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression("'Any string'");
+        String result = (String) expression.getValue();
+        System.out.println(result);
 
-                expression = expressionParser.parseExpression("'Any string'.length()");
-                Integer result2 = (Integer) expression.getValue();
-                System.out.println(result2);
+        expression = expressionParser.parseExpression("'Any string'.length()");
+        Integer result2 = (Integer) expression.getValue();
+        System.out.println(result2);
 
-                Countries countries = new Countries("CC", "Chile", Short.valueOf("2"));
-                expression = expressionParser.parseExpression("countryName");
-                EvaluationContext context = new StandardEvaluationContext(countries);
-                String result3 = (String) expression.getValue(context);
-                System.out.println(result3);
+        Countries countries = new Countries("CC", "Chile", Short.valueOf("2"));
+        expression = expressionParser.parseExpression("countryName");
+        EvaluationContext context = new StandardEvaluationContext(countries);
+        String result3 = (String) expression.getValue(context);
+        System.out.println(result3);
 
-                context = new StandardEvaluationContext();
-                context.setVariable("country", countries);
-                context.setVariable("todos",
-                                Lists.list(new Todo("a", "hello"), new Todo("b", "123"), new Todo("c", "opc")));
-                expression = expressionParser.parseExpression("#country.countryName == 'Chile' and #todos.size > 0");
-                boolean result4 = (Boolean) expression.getValue(context);
-                System.out.println(result4);
+        context = new StandardEvaluationContext();
+        context.setVariable("country", countries);
+        context.setVariable("todos",
+                Lists.list(new Todo("a", "hello"), new Todo("b", "123"), new Todo("c", "opc")));
+        expression = expressionParser.parseExpression("#country.countryName == 'Chile' and #todos.size > 0");
+        boolean result4 = (Boolean) expression.getValue(context);
+        System.out.println(result4);
 
-                // Lists.list().stream().filter(item -> return true).
-                // expression = expressionParser.parseExpression("#todos.size > 0 and
-                // #todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c2'][0].name == 'opc'");
-                expression = expressionParser.parseExpression(
-                                "#todos.size > 0 and #todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c2'][0].name == 'opc'");
-                boolean result5 = (Boolean) expression.getValue(context);
-                System.out.println(result5);
+        // expression = expressionParser.parseExpression("#todos.size > 0 and
+        // #todos.?[id == 'c2'].get(0)?.name == 'opc'");
+        expression = expressionParser.parseExpression(
+                "#todos.size > 0 and #todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c2'][0].name == 'opc'");
+        boolean result5 = (Boolean) expression.getValue(context);
+        System.out.println(result5);
 
-                expression = expressionParser.parseExpression(
-                                "#todos.size > 0 ? (#todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c'][0].name == 'opc' ? 1 : (#todos.?[id == 'b2'].size > 0 and #todos.?[id == 'b'][0].name == '123' ? 2 : 3)) : 0");
-                Integer result6 = (Integer) expression.getValue(context);
-                System.out.println(result6);
+        // filter
+        expression = expressionParser.parseExpression(
+                "#todos.size > 0 ? (#todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c'][0].name == 'opc' ? 1 : (#todos.?[id == 'b2'].size > 0 and #todos.?[id == 'b'][0].name == '123' ? 2 : 3)) : 0");
+        Integer result6 = (Integer) expression.getValue(context);
+        System.out.println(result6);
+        // first
+        expression = expressionParser.parseExpression(
+                "#todos.size > 0 ? (#todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c'][0].name == 'opc' ? 1 : (#todos.?[id == 'b2'].size > 0 and #todos.^[id == 'b'][0].name == '123' ? 2 : 3)) : 0");
+        result6 = (Integer) expression.getValue(context);
+        System.out.println(result6);
+        // last
+        expression = expressionParser.parseExpression(
+                "#todos.size > 0 ? (#todos.?[id == 'c2'].size > 0 and #todos.?[id == 'c'][0].name == 'opc' ? 1 : (#todos.?[id == 'b2'].size > 0 and #todos.$[id == 'b'][0].name == '123' ? 2 : 3)) : 0");
+        result6 = (Integer) expression.getValue(context);
+        System.out.println(result6);
 
-                assertTrue(true);
-        }
+        context.setVariable("dateTime", LocalDateTime.now());
+        log.info(LocalDateTime.now().getClass().getCanonicalName());
+        expression = expressionParser.parseExpression(
+                "#dateTime instanceof T(java.time.LocalDateTime)");
+        Boolean result7 = (Boolean) expression.getValue(context);
+        System.out.println(result7);
 
-        @Data
-        @AllArgsConstructor
-        public static class Todo {
-                String id;
-                String name;
-        }
+        expression = expressionParser.parseExpression(
+                "#systemEnvironment");
+        System.out.println(expression.getValue(java.util.Properties.class));
+
+        expression = expressionParser.parseExpression(
+                "#todos.![name]");
+        System.out.println(expression.getValue(context, java.util.List.class));
+        expression = expressionParser.parseExpression(
+                "#todos.![name matches '\\d+']");
+        System.out.println(expression.getValue(context, java.util.List.class));
+
+        assertTrue(true);
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Todo {
+        String id;
+        String name;
+    }
 }
